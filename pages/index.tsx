0320c3a7a01dcdf677a8cba3a538158cf04e0164
Samir Ergaibi <samirergaibi@gmail.com>
2021-12-18 18:55:03 +0100

    fix: add Spartan as a font

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
 import type { NextPage } from "next";
+import styled from "styled-components";
 import SignupForm from "../components/SignupForm";
 import LoginForm from "../components/LoginForm";
 
+const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 const Home: NextPage = () => {
   return (
-    <>
+    <StyledWrapper>
       <h1>GymStats 💪</h1>
       <h2>Login</h2>
       <LoginForm />
@@ -13,7 +20,7 @@ const Home: NextPage = () => {
 
       <h2>Sign up</h2>
       <SignupForm />
-    </>
+    </StyledWrapper>
   );
 };
 
