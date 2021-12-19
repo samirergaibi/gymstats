a1fe1ffd3242432184dbcaa585b8f1cb6b8538be
Samir Ergaibi <samirergaibi@gmail.com>
2021-12-19 17:43:16 +0100

    fix: reset css and add css variables constant

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,35 @@ import type { AppProps } from "next/app";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
 import Layout from "../components/Layout";
 
+const cssVariables = `
+  :root {
+    --box-shadow-main: 0 15px 35px 0 rgba(60, 66, 87, 0.08),
+    0 5px 15px 0 rgba(0, 0, 0, 0.12);
+  }
+`;
+
 const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
+  ${cssVariables}
+  * {
     box-sizing: border-box;
+    margin: 0;
     font-family: 'Spartan', sans-serif;
+  }
+  
+  html, body, #__next {
+    height: 100%;
+  }
+
+  body {
+    line-height: 1.5;
+  }
+
+  input, button, textarea, select {
+    font: inherit;
+  }
+
+  p, h1, h2, h3, h4, h5, h6 {
+    overflow-wrap: break-word;
   }
 `;
 
