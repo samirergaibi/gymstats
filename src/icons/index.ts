c25fce18df9ea5b7d82d08010ef783329c4bba56
Samir Ergaibi <samirergaibi@gmail.com>
2022-05-21 14:49:51 +0200

    feat: add edit icon

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,6 +1,7 @@
 export { default as ArrowDownCircleIcon } from './ArrowDownCircleIcon';
 export { default as ArrowRightIcon } from './ArrowRightIcon';
 export { default as DumbellIcon } from './DumbellIcon';
+export { default as EditIcon } from './EditIcon';
 export { default as FeatherIcon } from './FeatherIcon';
 export { default as RepetitionIcon } from './RepetitionIcon';
 export { default as SetsIcon } from './SetsIcon';
