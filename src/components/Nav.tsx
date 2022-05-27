50c07823d03b9814aaf06be7c2d469432afe72d7
Samir Ergaibi <samirergaibi@gmail.com>
2022-05-27 20:59:03 +0200

    fix: also remove about page from beta

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -42,7 +42,6 @@ async function logout() {
 
 const loggedOutRoutes = [
   { href: Paths.ROOT, text: 'Hem' },
-  { href: Paths.ABOUT, text: 'Om Gymstats' },
   { href: Paths.LOGIN, text: 'Logga in' },
 ];
 
